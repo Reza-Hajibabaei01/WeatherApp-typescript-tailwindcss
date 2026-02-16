4dd49c5165cb19efa10bbca881acade86fb123b3
Reza Hajibabaei <rezahajibabaei2001@gmail.com>
2026-02-16 22:42:47 +0330

    install the clerk-react package

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import Navbar from "./components/Navbar";
 import MainLayout from "./layouts/MainLayout";
+import { ClerkProvider } from '@clerk/clerk-react'
 
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
 
 function App() {
   return (
