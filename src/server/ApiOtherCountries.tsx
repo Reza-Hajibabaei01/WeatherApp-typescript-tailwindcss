f30a4425a2fe3d1342ee8e8cb3eabe7955399fbf
Reza Hajibabaei <rezahajibabaei2001@gmail.com>
2026-02-27 14:14:03 +0330

    Create the Other Countries Api component

---
+++ src/server/ApiOtherCountries.tsx
@@ -0,0 +1,19 @@
+// services/weatherApi.ts
+import type { WeatherData } from "../type/type";
+
+const API_KEY = "dec5e7b01cbb0354933380de97939855";
+
+export const fetchWeatherByCoords = async (
+  lat: number,
+  lon: number,
+): Promise<WeatherData> => {
+  const res = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`,
+  );
+
+  if (!res.ok) {
+    throw new Error("Weather fetch failed");
+  }
+
+  return res.json();
+};
